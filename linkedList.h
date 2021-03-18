22048f9588d8f5473b4cbad0961b2bc86bb26a79
Tim Tucker <varlogtim@gmail.com>
2021-03-17 18:52:11 -0700

    Formatting updates.
    Added some formatting and made a few notes.

--- linkedList.h
+++ linkedList.h
@@ -1,75 +1,46 @@
 #include <bits/stdc++.h>
 
-template <class T>
-class Node {
+template <class T> class Node {
   public:
-    Node(T v) {
-        _val = v;
-    };
-    ~Node() {
-        // code for linking prev and next
-        // link prev node (left)
-        if (_prev == nullptr && _next == nullptr) {
-            return; // Do nothing, list is one entry
-        }
-        if (_prev != nullptr) {
-            _prev->setNext(_next);
-        }
-        if (_next != nullptr) {
-            _next->setPrev(_prev);
-        }
-    }
-    Node *getNext() {
-        return _next;
-    }
-    Node *getPrev() {
-        return _prev;
-    }
-    void setNext(Node *n) {
-        _next = n;
-    }
-    void setPrev(Node *n) {
-        _prev = n;
-    }
-    T getVal() {
-        return _val;
-    };
-    void setVal(T v) {
-        _val = v;
-    };
+    Node(T v);
+    ~Node();
+
+    Node<T> *getNext();
+    Node<T> *getPrev();
+    void setNext(Node<T> *n);
+    void setPrev(Node<T> *n);
+    T getVal();
+    void setVal(T v);
+
   private:
-    T _val;
-    Node *_next = nullptr;
-    Node *_prev = nullptr;
+    T val_;
+    Node<T> *next_ = nullptr;
+    Node<T> *prev_ = nullptr;
 };
 
-
 // Doubly linked list impl.
-template <class T>
-class LinkedList 
-{
+template <class T> class LinkedList {
   public:
     LinkedList();
-    ~LinkedList() {
-        Node<T> *node = _head;
-        Node<T> *curNode = node;
-        while(node->getNext() != nullptr) {
-            node = curNode->getNext();
-            delete curNode;
-            _numNodes--;  // debug
-        }
-    };
+    ~LinkedList();
+
     void push(T v);
     void pushRight(T v);
     void pushLeft(T v);
     T pop();
     T popRight();
     T popLeft();
+    // T peek();
+    // T peekRight();
+    // T peekLeft();
+    // T getIndex();
+    // T popIndex();
     uint64_t getLength();
     std::string str();
+
   private:
     void initFirst(Node<T> *n);
-    Node<T> *_head = nullptr;
-    Node<T> *_tail = nullptr;
-    uint64_t _numNodes = 0;
+    Node<T> *head_ = nullptr;
+    Node<T> *tail_ = nullptr;
+    uint64_t numNodes_ = 0;
 };
